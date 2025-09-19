a16ae8076fb3ee6a3342b7c18ac2a1344411e267
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-09-20 01:11:34 +0300

    implement Transaction entity

---
+++ domain/transaction.go
@@ -0,0 +1,42 @@
+package domain
+
+import "time"
+
+type TypeTransaction string
+
+const (
+	Issuance TypeTransaction = "Issuance"
+	Return   TypeTransaction = "Return"
+)
+
+// Transaction представляет запись о выдаче или возврате инструментов
+type Transaction struct {
+	Id               int64
+	UserId           int64             // Received в UI, у кого инструмент
+	Type             TypeTransaction   // тип транзакции
+	IssuedAt         time.Time         // дата выдачи
+	ExpectedReturnAt time.Time         // ожидаемая дата возврата
+	ReturnedAt       time.Time         // фактическая дата возврата
+	Tools            []TransactionTool // вложенный список инструментов
+}
+
+// TODO: если по БЛ известно, когда возвращают инструменты
+// то логичнее заполнять автоматически через код/бд
+func NewTransaction(userId int64, typeTransaction TypeTransaction, ExpectedReturnAt, ReturnedAt time.Time) *Transaction {
+	return &Transaction{
+		UserId:           userId,
+		Type:             typeTransaction,
+		ExpectedReturnAt: ExpectedReturnAt,
+		ReturnedAt:       ReturnedAt,
+	}
+}
+
+func (t *Transaction) AddTool(toolId int64, qty int) {
+	tool := TransactionTool{
+		TransactionID: t.Id,
+		ToolID:        toolId,
+		Qty:           qty,
+	}
+
+	t.Tools = append(t.Tools, tool)
+}
