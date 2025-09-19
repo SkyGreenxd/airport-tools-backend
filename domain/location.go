4a543be4f9181947d321099d9ad797293161cc8a
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-09-20 01:22:44 +0300

    implement Station, Store, Location entities

--- domain/location.go
+++ domain/location.go
@@ -1,12 +1,41 @@
 package domain
 
-// FIXME: переработать, таблица не так будет выглядеть
-// мб таблица будет ссылаться сама на себя
-// тут иерархия аэропорт->склад->ячейка
-// Location описывает местоположение конкретного инструмента
+// Station описывает аэропорт
+type Station struct {
+	Id   int64
+	Code string // код аэропорта IATA = 3 буквы (на скрине нет, ICAO = 4 буквы)
+}
+
+// Store описывает склад
+type Store struct {
+	Id        int64
+	StationId int64
+	Name      string
+}
+
+// Location описывает ячейку на складе
 type Location struct {
-	Id       int64
-	Station  string // код аэропорта IATA = 3 буквы (на скрине нет, ICAO = 4 буквы)
-	Store    string // склад
-	Location string // ячейка
+	Id      int64
+	StoreId int64
+	Name    string
+}
+
+func NewStation(code string) *Station {
+	return &Station{
+		Code: code,
+	}
+}
+
+func NewStore(stationId int64, name string) *Store {
+	return &Store{
+		StationId: stationId,
+		Name:      name,
+	}
+}
+
+func NewLocation(storeId int64, name string) *Location {
+	return &Location{
+		StoreId: storeId,
+		Name:    name,
+	}
 }
