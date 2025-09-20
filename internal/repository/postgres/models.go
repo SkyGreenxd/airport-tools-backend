32d1392b2b2dd951f4d2988b67f542ed531a0a10
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-09-21 01:47:12 +0300

    implement models with gorm tags

--- internal/repository/postgres/models.go
+++ internal/repository/postgres/models.go
@@ -5,22 +5,29 @@ import (
 	"time"
 )
 
-// TODO: добавить GORM теги
 type StationModel struct {
 	Id   int64
 	Code string
-}
 
-type LocationModel struct {
-	Id      int64
-	StoreId int64
-	Name    string
+	Stores []*StoreModel `gorm:"foreignKey:StationId"`
 }
 
 type StoreModel struct {
 	Id        int64
 	StationId int64
 	Name      string
+
+	Station   *StationModel    `gorm:"foreignKey:StationId"`
+	Locations []*LocationModel `gorm:"foreignKey:StoreId"`
+}
+
+type LocationModel struct {
+	Id      int64
+	StoreId int64
+	Name    string
+
+	Store *StoreModel  `gorm:"foreignKey:StoreId"`
+	Tools []*ToolModel `gorm:"foreignKey:LocationId"`
 }
 
 type ToolModel struct {
@@ -29,16 +36,20 @@ type ToolModel struct {
 	ToirId     int64
 	LocationId int64
 	SnBn       string
-	ExpiresAt  time.Time
+	ExpiresAt  *time.Time
+
+	ToolType *ToolTypeModel `gorm:"foreignKey:TypeToolId"`
+	Location *LocationModel `gorm:"foreignKey:LocationId"`
 }
 
-type ToolType struct {
+type ToolTypeModel struct {
 	Id          int64
 	PartNumber  string
-	Name        string
 	Description string
 	Co          string
 	MC          string
+
+	Tools []*ToolTypeModel `gorm:"foreignKey:TypeToolId"`
 }
 
 type TransactionModel struct {
@@ -48,14 +59,18 @@ type TransactionModel struct {
 	IssuedAt         time.Time
 	ExpectedReturnAt time.Time
 	ReturnedAt       *time.Time
-	Tools            []domain.TransactionTool
+
+	User  *UserModel              `gorm:"foreignKey:UserId"`
+	Tools []*TransactionToolModel `gorm:"foreignKey:TransactionID"`
 }
 
 type TransactionToolModel struct {
 	ID            int64
 	TransactionID int64
 	ToolID        int64
-	Qty           int
+
+	Transaction TransactionModel `gorm:"foreignKey:TransactionID"`
+	Tool        ToolModel        `gorm:"foreignKey:ToolID"`
 }
 
 type UserModel struct {
@@ -63,4 +78,38 @@ type UserModel struct {
 	EmployeeId string
 	FullName   string
 	Role       domain.Role
+
+	Transactions []*TransactionModel `gorm:"foreignKey:UserId"`
+}
+
+func (UserModel) TableName() string {
+	return "users"
+}
+
+func (TransactionToolModel) TableName() string {
+	return "transactions_tools"
+}
+
+func (TransactionModel) TableName() string {
+	return "transactions"
+}
+
+func (ToolTypeModel) TableName() string {
+	return "tool_types"
+}
+
+func (ToolModel) TableName() string {
+	return "tools"
+}
+
+func (StoreModel) TableName() string {
+	return "stores"
+}
+
+func (LocationModel) TableName() string {
+	return "locations"
+}
+
+func (StationModel) TableName() string {
+	return "stations"
 }
