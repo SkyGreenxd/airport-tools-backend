4e530b680540836ed29778b7a707119b7130e8b2
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-09-20 22:54:59 +0300

    add new constructor

--- internal/domain/transaction_tool.go
+++ internal/domain/transaction_tool.go
@@ -2,10 +2,15 @@ package domain
 
 // TransactionTool связывает транзакцию с инструментами.
 // Хранит количество выданного инструмента (Qty).
-// TODO: мб перенести сущность к Transaction
 type TransactionTool struct {
 	ID            int64
 	TransactionID int64 // fk Transaction
 	ToolID        int64 // fk Tool
-	Qty           int   // сколько выдали
+}
+
+func NewTransactionTool(transactionID, toolID int64) *TransactionTool {
+	return &TransactionTool{
+		TransactionID: transactionID,
+		ToolID:        toolID,
+	}
 }
