8c4f66507139957305ebbf30bb591784dc43408d
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-09-21 02:20:09 +0300

    add new errors

--- pkg/e/e.go
+++ pkg/e/e.go
@@ -5,6 +5,12 @@ import "fmt"
 var (
 	ErrStationNotFound = fmt.Errorf("station not found")
 	ErrStationExists   = fmt.Errorf("station is exists")
+
+	ErrStoreNotFound = fmt.Errorf("store not found")
+	ErrStoreExists   = fmt.Errorf("store exists")
+
+	ErrLocationNotFound = fmt.Errorf("location not found")
+	ErrLocationExists   = fmt.Errorf("location is exists")
 )
 
 func Wrap(msg string, err error) error {
