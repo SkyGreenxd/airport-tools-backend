ac1a2fd75422cce8b3b23f978171a5194f3d2be9
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-09-22 02:58:09 +0300

    add new errors

--- pkg/e/e.go
+++ pkg/e/e.go
@@ -11,6 +11,25 @@ var (
 
 	ErrLocationNotFound = fmt.Errorf("location not found")
 	ErrLocationExists   = fmt.Errorf("location is exists")
+
+	ErrToolNotFound = fmt.Errorf("tool not found")
+	ErrToolExists   = fmt.Errorf("tool is exists")
+
+	ErrToolTypeNotFound = fmt.Errorf("tool type not found")
+	ErrToolTypeExists   = fmt.Errorf("tool type exists")
+	ErrToolTypeIsUsed   = fmt.Errorf("tool type is used")
+
+	ErrTransactionNotFound = fmt.Errorf("transaction not found")
+
+	ErrTransactionToolNotFound = fmt.Errorf("there is no such entry in the table")
+	ErrTransactionToolExists   = fmt.Errorf("such an instrument already exists in the transaction")
+
+	ErrUserNotFound = fmt.Errorf("user not found")
+	ErrUserExists   = fmt.Errorf("user is exists")
+	ErrUserInUse    = fmt.Errorf("the user has outstanding transactions")
+
+	ErrNothingToChange = fmt.Errorf("nothing to change")
+	ErrIncorrectDate   = fmt.Errorf("incorrect date")
 )
 
 func Wrap(msg string, err error) error {
