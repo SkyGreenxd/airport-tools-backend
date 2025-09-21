8ef8903071e4334b8b0c1be12a4847722ece47ca
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-09-22 02:59:11 +0300

    add nested structures and ChangeName func

--- internal/domain/location.go
+++ internal/domain/location.go
@@ -1,10 +1,15 @@
 package domain
 
+import "airport-tools-backend/pkg/e"
+
 // Location описывает ячейку на складе
 type Location struct {
 	Id      int64
 	StoreId int64
 	Name    string
+
+	StoreObj *Store
+	Tools    []*Tool
 }
 
 func NewLocation(storeId int64, name string) *Location {
@@ -12,4 +17,13 @@ func NewLocation(storeId int64, name string) *Location {
 		StoreId: storeId,
 		Name:    name,
 	}
+}
+
+func (l *Location) ChangeName(name string) error {
+	if l.Name == name {
+		return e.ErrNothingToChange
+	}
+
+	l.Name = name
+	return nil
 }
