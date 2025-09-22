5a05881707ed4de87600024090d226afea868135
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-09-22 03:00:28 +0300

    add nested structures and new func ChangeCode

--- internal/domain/station.go
+++ internal/domain/station.go
@@ -1,13 +1,26 @@
 package domain
 
+import "airport-tools-backend/pkg/e"
+
 // Station описывает аэропорт
 type Station struct {
 	Id   int64
 	Code string // код аэропорта IATA = 3 буквы (на скрине нет, ICAO = 4 буквы)
+
+	Stores []*Store
 }
 
 func NewStation(code string) *Station {
 	return &Station{
 		Code: code,
 	}
+}
+
+func (s *Station) ChangeCode(code string) error {
+	if s.Code == code {
+		return e.ErrNothingToChange
+	}
+
+	s.Code = code
+	return nil
 }
