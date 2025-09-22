675385d535a9edb8635977d1c762045735f0995c
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-09-22 03:01:53 +0300

    add nested structures and add new functions

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -1,5 +1,7 @@
 package domain
 
+import "airport-tools-backend/pkg/e"
+
 type Role string
 
 const (
@@ -9,11 +11,14 @@ const (
 )
 
 // User описывает пользователя
+// TODO: мб стоит привязать пользователя к аэропорту
 type User struct {
 	Id         int64
 	EmployeeId string // табельный номер
 	FullName   string // фио
 	Role       Role   // роль пользователя
+
+	Transactions []*Transaction
 }
 
 func NewUser(employeeId, fullName, role string) *User {
@@ -32,4 +37,22 @@ func ValidateRole(r Role) bool {
 	}
 
 	return false
+}
+
+func (u *User) ChangeFullName(newFullName string) error {
+	if u.FullName == newFullName {
+		return e.ErrNothingToChange
+	}
+
+	u.FullName = newFullName
+	return nil
+}
+
+func (u *User) ChangeRole(r Role) error {
+	if u.Role == r {
+		return e.ErrNothingToChange
+	}
+
+	u.Role = r
+	return nil
 }
