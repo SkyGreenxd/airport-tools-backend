e45c0c2e8d5ddeba8092df8ec58f01f44c36a0dd
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-09-22 03:00:11 +0300

    add new functions

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -11,6 +11,7 @@ type StationRepository interface {
 	GetAll(ctx context.Context) ([]*domain.Station, error)
 	Delete(ctx context.Context, id int64) error
 	Update(ctx context.Context, station *domain.Station) (*domain.Station, error)
+	GetByIdWithStores(ctx context.Context, id int64) (*domain.Station, error)
 }
 
 type StoreRepository interface {
@@ -19,6 +20,8 @@ type StoreRepository interface {
 	GetAll(ctx context.Context) ([]*domain.Store, error)
 	Delete(ctx context.Context, id int64) error
 	Update(ctx context.Context, store *domain.Store) (*domain.Store, error)
+	GetByIdWithStation(ctx context.Context, id int64) (*domain.Store, error)
+	GetByIdWithLocations(ctx context.Context, id int64) (*domain.Store, error)
 }
 
 type LocationRepository interface {
@@ -27,6 +30,7 @@ type LocationRepository interface {
 	GetAll(ctx context.Context) ([]*domain.Location, error)
 	Delete(ctx context.Context, id int64) error
 	Update(ctx context.Context, location *domain.Location) (*domain.Location, error)
+	GetByIdWithTools(ctx context.Context, id int64) (*domain.Location, error)
 }
 
 type ToolRepository interface {
@@ -35,6 +39,9 @@ type ToolRepository interface {
 	GetAll(ctx context.Context) ([]*domain.Tool, error)
 	Delete(ctx context.Context, id int64) error
 	Update(ctx context.Context, tool *domain.Tool) (*domain.Tool, error)
+	GetByIdWithToolType(ctx context.Context, toolId int64) (*domain.Tool, error)
+	GetByIdWithLocation(ctx context.Context, toolId int64) (*domain.Tool, error)
+	GetByIdWithAllData(ctx context.Context, toolId int64) (*domain.Tool, error)
 }
 
 type ToolTypeRepository interface {
@@ -43,29 +50,34 @@ type ToolTypeRepository interface {
 	GetAll(ctx context.Context) ([]*domain.ToolType, error)
 	Delete(ctx context.Context, id int64) error
 	Update(ctx context.Context, toolType *domain.ToolType) (*domain.ToolType, error)
+	GetByIdWithTools(ctx context.Context, id int64) (*domain.ToolType, error)
 }
 
 type TransactionRepository interface {
-	Create(ctx context.Context, station *domain.Transaction) (*domain.Transaction, error)
+	Create(ctx context.Context, transaction *domain.Transaction) (*domain.Transaction, error)
 	GetById(ctx context.Context, id int64) (*domain.Transaction, error)
+	GetByIdWithTools(ctx context.Context, id int64) (*domain.Transaction, error)
+	GetByIdWithUser(ctx context.Context, id int64) (*domain.Transaction, error)
 	GetAll(ctx context.Context) ([]*domain.Transaction, error)
 	Delete(ctx context.Context, id int64) error
-	Update(ctx context.Context, station *domain.Transaction) (*domain.Transaction, error)
+	Update(ctx context.Context, transaction *domain.Transaction) (*domain.Transaction, error)
 }
 
 type TransactionToolRepository interface {
-	Create(ctx context.Context, station *domain.TransactionTool) (*domain.TransactionTool, error)
+	Create(ctx context.Context, transactionTool *domain.TransactionTool) (*domain.TransactionTool, error)
 	GetById(ctx context.Context, id int64) (*domain.TransactionTool, error)
 	GetByTransactionId(ctx context.Context, transactionId int64) ([]*domain.TransactionTool, error)
-	Update(ctx context.Context, station *domain.TransactionTool) (*domain.TransactionTool, error)
+	GetByToolId(ctx context.Context, toolId int64) ([]*domain.TransactionTool, error)
 	Delete(ctx context.Context, id int64) error
-	GetAll(ctx context.Context) ([]*domain.TransactionTool, error)
 	GetUnreturnedByTransactionID(ctx context.Context, transactionId int64) ([]*domain.TransactionTool, error)
 }
 
 type UserRepository interface {
 	Create(ctx context.Context, user *domain.User) (*domain.User, error)
 	GetById(ctx context.Context, id int64) (*domain.User, error)
+	GetByEmployeeId(ctx context.Context, employeeId int64) (*domain.User, error)
+	GetByFullName(ctx context.Context, fullName string) (*domain.User, error)
+	GetByIdWithTransactions(ctx context.Context, id int64) (*domain.User, error)
 	GetAll(ctx context.Context) ([]*domain.User, error)
 	Delete(ctx context.Context, id int64) error
 	Update(ctx context.Context, user *domain.User) (*domain.User, error)
