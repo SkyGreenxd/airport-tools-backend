93a21d54b729bf1977d5d6b414637bbcb0265433
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-09-22 03:01:19 +0300

    add nested structures and new func ChangeDescription

--- internal/domain/tool_type.go
+++ internal/domain/tool_type.go
@@ -1,20 +1,32 @@
 package domain
 
-// TODO: в модели для GORM добавить ф-цию func TableName() string
+import "airport-tools-backend/pkg/e"
+
 // ToolType описывает тип инструмента
 type ToolType struct {
 	Id          int64
 	PartNumber  string // партийный номер
 	Description string // описание
-	Co          string // состав/группа сплавов
-	MC          string // код марки/сплава
+	//Co          string // состав/группа сплавов
+	//MC          string // код марки/сплава
+
+	Tools []*Tool
 }
 
-func NewToolType(partNumber, description, co, mc string) *ToolType {
+func NewToolType(partNumber, description string) *ToolType {
 	return &ToolType{
 		PartNumber:  partNumber,
 		Description: description,
-		Co:          co,
-		MC:          mc,
+		//Co:          co,
+		//MC:          mc,
 	}
+}
+
+func (t *ToolType) ChangeDescription(description string) error {
+	if t.Description == description {
+		return e.ErrNothingToChange
+	}
+
+	t.Description = description
+	return nil
 }
