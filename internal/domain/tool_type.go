1e9af3b3b5205f779373b638f067500b8ca01035
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-09-25 02:50:28 +0300

    change ToolType entity

--- internal/domain/tool_type.go
+++ internal/domain/tool_type.go
@@ -4,29 +4,29 @@ import "airport-tools-backend/pkg/e"
 
 // ToolType описывает тип инструмента
 type ToolType struct {
-	Id          int64
-	PartNumber  string // партийный номер
-	Description string // описание
-	//Co          string // состав/группа сплавов
-	//MC          string // код марки/сплава
+	Id                 int64
+	PartNumber         string    // партийный номер
+	Name               string    // имя
+	ReferenceImageHash string    // хэш упражнения
+	ReferenceEmbedding []float32 // эмбеддинг
 
-	Tools []*Tool
+	ToolSets []*ToolSet
 }
 
-func NewToolType(partNumber, description string) *ToolType {
+func NewToolType(partNumber, name, referenceImageHash string, referenceEmbedding []float32) *ToolType {
 	return &ToolType{
-		PartNumber:  partNumber,
-		Description: description,
-		//Co:          co,
-		//MC:          mc,
+		PartNumber:         partNumber,
+		Name:               name,
+		ReferenceImageHash: referenceImageHash,
+		ReferenceEmbedding: referenceEmbedding,
 	}
 }
 
-func (t *ToolType) ChangeDescription(description string) error {
-	if t.Description == description {
+func (t *ToolType) ChangName(name string) error {
+	if t.Name == name {
 		return e.ErrNothingToChange
 	}
 
-	t.Description = description
+	t.Name = name
 	return nil
 }
