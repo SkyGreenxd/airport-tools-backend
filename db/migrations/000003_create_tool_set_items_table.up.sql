297c21c000d3dd5a55727e2aef137e10b77b4a47
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-09-25 00:36:29 +0300

    change migration

--- db/migrations/000003_create_tool_set_items_table.up.sql
+++ db/migrations/000003_create_tool_set_items_table.up.sql
@@ -1,5 +1,4 @@
 CREATE TABLE IF NOT EXISTS tool_set_items (
-    id BIGSERIAL PRIMARY KEY,
     tool_set_id BIGINT NOT NULL REFERENCES tool_sets(id) ON DELETE CASCADE,
     tool_type_id BIGINT NOT NULL REFERENCES tool_types(id) ON DELETE RESTRICT,
     UNIQUE (tool_set_id, tool_type_id)
