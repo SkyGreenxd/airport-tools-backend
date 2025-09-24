067c0bfdced249bf736e062737dc93ddd6314031
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-09-25 00:31:18 +0300

    change errors

--- pkg/e/e.go
+++ pkg/e/e.go
@@ -3,32 +3,21 @@ package e
 import "fmt"
 
 var (
-	ErrStationNotFound = fmt.Errorf("station not found")
-	ErrStationExists   = fmt.Errorf("station is exists")
-
-	ErrStoreNotFound = fmt.Errorf("store not found")
-	ErrStoreExists   = fmt.Errorf("store exists")
-
-	ErrLocationNotFound = fmt.Errorf("location not found")
-	ErrLocationExists   = fmt.Errorf("location is exists")
-
-	ErrToolNotFound = fmt.Errorf("tool not found")
-	ErrToolExists   = fmt.Errorf("tool is exists")
-
 	ErrToolTypeNotFound = fmt.Errorf("tool type not found")
 	ErrToolTypeExists   = fmt.Errorf("tool type exists")
 	ErrToolTypeIsUsed   = fmt.Errorf("tool type is used")
 
-	ErrTransactionNotFound = fmt.Errorf("transaction not found")
+	ErrToolSetNotFound = fmt.Errorf("tool set not found")
+	ErrToolSetExists   = fmt.Errorf("tool set exists")
 
-	ErrTransactionToolNotFound = fmt.Errorf("there is no such entry in the table")
-	ErrTransactionToolExists   = fmt.Errorf("such an instrument already exists in the transaction")
+	ErrTransactionNotFound = fmt.Errorf("transaction not found")
 
 	ErrUserNotFound = fmt.Errorf("user not found")
 	ErrUserExists   = fmt.Errorf("user is exists")
 	ErrUserInUse    = fmt.Errorf("the user has outstanding transactions")
 
-	ErrCvScanNotFound = fmt.Errorf("cv_scan not found")
+	ErrCvScanNotFound       = fmt.Errorf("cv_scan not found")
+	ErrCvScanDetailNotFound = fmt.Errorf("cv_scan detail not found")
 
 	ErrNothingToChange = fmt.Errorf("nothing to change")
 	ErrIncorrectDate   = fmt.Errorf("incorrect date")
