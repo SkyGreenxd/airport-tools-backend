3951cef6e9c8d252e81ca557f9374869643ba371
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-09-25 00:32:03 +0300

    add new repositories

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -5,93 +5,52 @@ import (
 	"context"
 )
 
-type StationRepository interface {
-	Create(ctx context.Context, station *domain.Station) (*domain.Station, error)
-	GetById(ctx context.Context, id int64) (*domain.Station, error)
-	GetAll(ctx context.Context) ([]*domain.Station, error)
-	Delete(ctx context.Context, id int64) error
-	Update(ctx context.Context, station *domain.Station) (*domain.Station, error)
-	GetByIdWithStores(ctx context.Context, id int64) (*domain.Station, error)
-}
-
-type StoreRepository interface {
-	Create(ctx context.Context, store *domain.Store) (*domain.Store, error)
-	GetById(ctx context.Context, id int64) (*domain.Store, error)
-	GetAll(ctx context.Context) ([]*domain.Store, error)
-	Delete(ctx context.Context, id int64) error
-	Update(ctx context.Context, store *domain.Store) (*domain.Store, error)
-	GetByIdWithStation(ctx context.Context, id int64) (*domain.Store, error)
-	GetByIdWithLocations(ctx context.Context, id int64) (*domain.Store, error)
-}
-
-type LocationRepository interface {
-	Create(ctx context.Context, location *domain.Location) (*domain.Location, error)
-	GetById(ctx context.Context, id int64) (*domain.Location, error)
-	GetAll(ctx context.Context) ([]*domain.Location, error)
-	Delete(ctx context.Context, id int64) error
-	Update(ctx context.Context, location *domain.Location) (*domain.Location, error)
-	GetByIdWithTools(ctx context.Context, id int64) (*domain.Location, error)
-}
-
-type ToolRepository interface {
-	Create(ctx context.Context, tool *domain.Tool) (*domain.Tool, error)
-	GetById(ctx context.Context, id int64) (*domain.Tool, error)
-	GetAll(ctx context.Context) ([]*domain.Tool, error)
-	Delete(ctx context.Context, id int64) error
-	Update(ctx context.Context, tool *domain.Tool) (*domain.Tool, error)
-	GetByIdWithToolType(ctx context.Context, toolId int64) (*domain.Tool, error)
-	GetByIdWithLocation(ctx context.Context, toolId int64) (*domain.Tool, error)
-	GetByIdWithAllData(ctx context.Context, toolId int64) (*domain.Tool, error)
-}
-
 type ToolTypeRepository interface {
 	Create(ctx context.Context, toolType *domain.ToolType) (*domain.ToolType, error)
 	GetById(ctx context.Context, id int64) (*domain.ToolType, error)
 	GetAll(ctx context.Context) ([]*domain.ToolType, error)
 	Delete(ctx context.Context, id int64) error
 	Update(ctx context.Context, toolType *domain.ToolType) (*domain.ToolType, error)
-	GetByIdWithTools(ctx context.Context, id int64) (*domain.ToolType, error)
 }
 
-type TransactionRepository interface {
-	Create(ctx context.Context, transaction *domain.Transaction) (*domain.Transaction, error)
-	GetById(ctx context.Context, id int64) (*domain.Transaction, error)
-	GetByIdWithTools(ctx context.Context, id int64) (*domain.Transaction, error)
-	GetByIdWithUser(ctx context.Context, id int64) (*domain.Transaction, error)
-	GetAll(ctx context.Context) ([]*domain.Transaction, error)
+type ToolSetRepository interface {
+	Create(ctx context.Context, toolSet *domain.ToolSet) (*domain.ToolSet, error)
+	GetById(ctx context.Context, id int64) (*domain.ToolSet, error)
+	GetAll(ctx context.Context) ([]*domain.ToolSet, error)
 	Delete(ctx context.Context, id int64) error
-	Update(ctx context.Context, transaction *domain.Transaction) (*domain.Transaction, error)
-}
-
-type TransactionToolRepository interface {
-	Create(ctx context.Context, transactionTool *domain.TransactionTool) (*domain.TransactionTool, error)
-	GetById(ctx context.Context, id int64) (*domain.TransactionTool, error)
-	GetByTransactionId(ctx context.Context, transactionId int64) ([]*domain.TransactionTool, error)
-	GetByToolId(ctx context.Context, toolId int64) ([]*domain.TransactionTool, error)
-	Delete(ctx context.Context, id int64) error
-	GetUnreturnedByTransactionID(ctx context.Context, transactionId int64) ([]*domain.TransactionTool, error)
+	Update(ctx context.Context, toolSet *domain.ToolSet) (*domain.ToolSet, error)
+	GetByIdWithTools(ctx context.Context, id int64) (*domain.ToolSet, error)
 }
 
 type UserRepository interface {
 	Create(ctx context.Context, user *domain.User) (*domain.User, error)
 	GetById(ctx context.Context, id int64) (*domain.User, error)
-	GetByEmployeeId(ctx context.Context, employeeId int64) (*domain.User, error)
-	GetByFullName(ctx context.Context, fullName string) (*domain.User, error)
 	GetByIdWithTransactions(ctx context.Context, id int64) (*domain.User, error)
 	GetAll(ctx context.Context) ([]*domain.User, error)
 	Delete(ctx context.Context, id int64) error
 	Update(ctx context.Context, user *domain.User) (*domain.User, error)
 }
 
+type TransactionRepository interface {
+	Create(ctx context.Context, transaction *domain.Transaction) (*domain.Transaction, error)
+	GetById(ctx context.Context, id int64) (*domain.Transaction, error)
+	GetByIdWithCvScans(ctx context.Context, id int64) (*domain.Transaction, error)
+	GetByIdWithUser(ctx context.Context, id int64) (*domain.Transaction, error)
+	GetAll(ctx context.Context) ([]*domain.Transaction, error)
+	Delete(ctx context.Context, id int64) error
+	Update(ctx context.Context, transaction *domain.Transaction) (*domain.Transaction, error)
+}
+
 type CvScanRepository interface {
 	Create(ctx context.Context, cvScan *domain.CvScan) (*domain.CvScan, error)
 	GetById(ctx context.Context, id int64) (*domain.CvScan, error)
 	GetByTransactionId(ctx context.Context, transactionId int64) (*domain.CvScan, error)
-	Update(ctx context.Context, cvScan *domain.CvScan) (*domain.CvScan, error)
+	GetByIdWithTransaction(ctx context.Context, id int64) (*domain.CvScan, error)
+	GetByIdWithDetectedTools(ctx context.Context, id int64) (*domain.CvScan, error)
 }
 
-// TODO: понять с помощью чего я достаю фотку (id, bucket, url??)
-type PhotoRepository interface {
-	Create(ctx context.Context, photo *domain.Image) (*domain.Image, error)
-	GetById(ctx context.Context, id uint64) (*domain.Image, error)
+type CvScanDetailRepository interface {
+	Create(ctx context.Context, cvScanDetail *domain.CvScanDetail) (*domain.CvScanDetail, error)
+	GetById(ctx context.Context, id int64) (*domain.CvScanDetail, error)
+	GetByCvScanId(ctx context.Context, cvScanId int64) ([]*domain.CvScanDetail, error)
 }
