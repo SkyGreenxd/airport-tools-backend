af893adf4bb68e60c8df80776ce16840042bcdf1
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-09-26 02:16:23 +0300

    add Confidence

--- internal/domain/cv_scan_detail.go
+++ internal/domain/cv_scan_detail.go
@@ -1,18 +1,20 @@
 package domain
 
+// CvScanDetail сущность создана для работы с бд
 type CvScanDetail struct {
 	Id                 int64
 	CvScanId           int64
 	DetectedToolTypeId int64
+	Confidence         float32
 	ImageHash          string
 	Embedding          []float32
 }
 
-func NewCvScanDetail(cvScanId, detectedToolTypeId int64, imageHash string, embedding []float32) *CvScanDetail {
+func NewCvScanDetail(cvScanId, detectedToolTypeId int64, confidence float32, imageHash string, embedding []float32) *CvScanDetail {
 	return &CvScanDetail{
-		Id:                 cvScanId,
 		CvScanId:           cvScanId,
 		DetectedToolTypeId: detectedToolTypeId,
+		Confidence:         confidence,
 		ImageHash:          imageHash,
 		Embedding:          embedding,
 	}
