43d9fd1467f328989aa1f2568a541897bb2f4696
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-09-26 19:31:42 +0300

    add new errors

--- pkg/e/e.go
+++ pkg/e/e.go
@@ -13,7 +13,8 @@ var (
 	ErrToolSetNotFound = fmt.Errorf("tool set not found")
 	ErrToolSetExists   = fmt.Errorf("tool set exists")
 
-	ErrTransactionNotFound = fmt.Errorf("transaction not found")
+	ErrTransactionNotFound   = fmt.Errorf("transaction not found")
+	ErrTransactionUnfinished = fmt.Errorf("you have an unfinished issue")
 
 	ErrUserNotFound = fmt.Errorf("user not found")
 	ErrUserExists   = fmt.Errorf("user is exists")
