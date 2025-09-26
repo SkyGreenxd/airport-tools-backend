37adb33978285198654471a2f26dad218c57e87c
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-09-27 01:49:05 +0300

    add swagger annotations

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,6 +2,13 @@ package main
 
 import "airport-tools-backend/internal/app"
 
+//	@title			Airport Tools Backend API
+//	@version		1.0
+//	@description	API for managing airport tools
+//	@termsOfService	http://swagger.io/terms/
+
+// @host		localhost:8080
+// @BasePath	/api/v1
 func main() {
 	app.Run()
 }
