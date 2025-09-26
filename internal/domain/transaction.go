29e6b44498f6c5bbb897f312de1c4d11fa6a0b7c
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-09-26 18:45:52 +0300

    add const for Reason

--- internal/domain/transaction.go
+++ internal/domain/transaction.go
@@ -3,11 +3,15 @@ package domain
 // Transaction представляет запись о выдаче или возврате инструментов
 
 type Status string
+type Reason string
 
 const (
 	OPEN   Status = "OPEN"
 	CLOSED Status = "CLOSED"
 	MANUAL Status = "MANUAL VERIFICATION"
+
+	RETURNED Reason = "All instruments have been handed over"
+	PROBLEMS Reason = "There are problems with the tools, a manual check is needed"
 )
 
 type Transaction struct {
@@ -15,7 +19,7 @@ type Transaction struct {
 	UserId    int64 // Received в UI, у кого инструмент
 	ToolSetId int64
 	Status    Status
-	Reason    *string
+	Reason    *Reason
 
 	User    *User
 	CvScans []*CvScan
