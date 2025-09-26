295d782c61338fce0956bfe11eda37d24763f4f5
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-09-26 18:44:21 +0300

    add new method

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -35,7 +35,8 @@ type UserRepository interface {
 type TransactionRepository interface {
 	Create(ctx context.Context, transaction *domain.Transaction) (*domain.Transaction, error)
 	GetById(ctx context.Context, id int64) (*domain.Transaction, error)
-	GetByUserId(ctx context.Context, userId string) (*domain.Transaction, error)
+	GetByUserId(ctx context.Context, userId int64) (*domain.Transaction, error)
+	GetByUserIdWhereStatusIsOpenOrManual(ctx context.Context, userId int64) (*domain.Transaction, error)
 	GetByIdWithCvScans(ctx context.Context, id int64) (*domain.Transaction, error)
 	GetByIdWithUser(ctx context.Context, id int64) (*domain.Transaction, error)
 	GetAll(ctx context.Context) ([]*domain.Transaction, error)
