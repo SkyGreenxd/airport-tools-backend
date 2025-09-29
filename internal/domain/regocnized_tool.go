e38bcc211fdf413075284588f4a08229ff8ac74e
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-09-30 02:33:06 +0300

    del hash

--- internal/domain/regocnized_tool.go
+++ internal/domain/regocnized_tool.go
@@ -1,17 +1,16 @@
 package domain
 
+// RecognizedTool описывает инструмент, который был распознан CV
 type RecognizedTool struct {
 	ToolTypeId int64
 	Confidence float32
-	HashTool   string
 	Embedding  []float32
 }
 
-func NewRecognizedTool(toolTypeId int64, confidence float32, hashTool string, embedding []float32) *RecognizedTool {
+func NewRecognizedTool(toolTypeId int64, confidence float32, embedding []float32) *RecognizedTool {
 	return &RecognizedTool{
 		ToolTypeId: toolTypeId,
 		Confidence: confidence,
-		HashTool:   hashTool,
 		Embedding:  embedding,
 	}
 }
