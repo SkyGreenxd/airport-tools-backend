0fd7f5e7e6ab7d16bf9c53fff40e2665f2abdbe1
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-10-16 21:09:43 +0300

    add new dto

--- internal/delivery/v1/dto.go
+++ internal/delivery/v1/dto.go
@@ -3,11 +3,49 @@ package v1
 import (
 	"airport-tools-backend/internal/domain"
 	"airport-tools-backend/internal/usecase"
+	"time"
 )
+
+type VerificationReq struct {
+	TransactionID int64  `json:"transaction_id" binding:"required"`
+	QAEmployeeId  string `json:"qa_employee_id" binding:"required"`
+	Notes         string `json:"notes"`
+}
+
+type VerificationRes struct {
+	TransactionID string    `json:"transaction_id"` // ID транзакции, которую QA завершил
+	Status        string    `json:"status"`         // Новый статус
+	VerifiedBy    string    `json:"verified_by"`    // Табельный номер или имя QA
+	VerifiedAt    time.Time `json:"verified_at"`    // Время завершения проверки
+	Message       string    `json:"message"`        // Краткое текстовое подтверждение
+}
+
+type ListTransactionsRes struct {
+	Transactions []domain.Transaction `json:"transactions"`
+}
+
+type RegisterReq struct {
+	EmployeeId string `json:"employee_id" binding:"required"`
+	FullName   string `json:"full_name" binding:"required"`
+	Role       string `json:"role" binding:"required"`
+}
+
+type RegisterRes struct {
+	Id string `json:"id"`
+}
+
+type LoginReq struct {
+	EmployeeId string `json:"employee_id" binding:"required"`
+}
+
+type LoginRes struct {
+	Role string `json:"role"`
+}
 
 type CheckReq struct {
 	EmployeeId string `json:"employee_id" binding:"required"`
 	Data       string `json:"data" binding:"required"`
+	ToolSetId  int64  `json:"tool_set_id"`
 }
 
 type CheckRes struct {
@@ -78,5 +116,6 @@ func ToUseCaseCheckReq(req *CheckReq) *usecase.CheckReq {
 	return &usecase.CheckReq{
 		EmployeeId: req.EmployeeId,
 		Data:       req.Data,
+		ToolSetId:  req.ToolSetId,
 	}
 }
