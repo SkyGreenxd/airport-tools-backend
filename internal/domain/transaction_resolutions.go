63f47ad6d1ea781cc7e674f39f2761591206453f
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-10-17 04:22:21 +0300

    add a condition

--- internal/domain/transaction_resolutions.go
+++ internal/domain/transaction_resolutions.go
@@ -1,3 +1,19 @@
 package domain
 
-// TODO: доделать
+import "time"
+
+type TransactionResolution struct {
+	Id            int64
+	TransactionId int64
+	QAEmployeeId  int64
+	Notes         string
+	CreatedAt     time.Time
+}
+
+func NewTransactionResolution(transactionId int64, qaEmployeeId int64, notes string) *TransactionResolution {
+	return &TransactionResolution{
+		TransactionId: transactionId,
+		QAEmployeeId:  qaEmployeeId,
+		Notes:         notes,
+	}
+}
